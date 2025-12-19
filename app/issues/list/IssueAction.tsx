59b84e7e72c39ac91739d4f018273a98a967dbd4
Micheal <michealbehailu94@gmail.com>
2025-12-19 11:45:54 +0300

    some changes

--- app/issues/list/IssueAction.tsx
+++ app/issues/list/IssueAction.tsx
@@ -4,14 +4,11 @@ import { Button, Flex, Link, Select } from "@radix-ui/themes";
 import React from "react";
 import IssueStatusFilter from "./IssueStatusFilter";
 
-
-
 const IssueAction = () => {
-  
   return (
     <Flex className="mb-5" justify={"between"}>
       <IssueStatusFilter />
-      <Button>
+      <Button asChild>
         <Link href="/issues/new">New Issue</Link>{" "}
       </Button>
     </Flex>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,11 @@
 import "@radix-ui/themes/styles.css";
 import "./theme-config.css";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Inter } from "next/font/google";
+import { Inter } from "next/font/google";
+import { GeistMono, GeistSans } from "geist/font";
 import "./globals.css";
 import NavBar from "./NavBar";
-import { Container, Theme, ThemePanel } from "@radix-ui/themes";
+import { Container, Theme } from "@radix-ui/themes";
 import AuthProvider from "./auth/Provider";
 import QueryClientProvider from "./QueryClientProvider";
 
@@ -12,16 +13,6 @@ const inter = Inter({
   subsets: ["latin"],
   display: "swap",
   variable: "--font-inter",
-});
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
@@ -36,7 +27,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.variable}>
+      <body
+        className={`${inter.variable} ${GeistSans.variable} ${GeistMono.variable}`}
+      >
         <QueryClientProvider>
           <AuthProvider>
             <Theme appearance="light" accentColor="violet">
